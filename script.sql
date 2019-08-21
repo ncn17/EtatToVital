4d557785206ad8675c3b492db4c1b8be977da9bb
ncn17 <nkounkounicephore@gmail.com>
2019-08-22 00:49:33 +0100

    Add cleaner Script

---
+++ script.sql
@@ -0,0 +1,17 @@
+﻿delete FROM vitalstatistics.chthibi_birth ;
+
+delete from vitalstatistics.chthibi_death ;
+
+delete FROM vitalstatistics.chthibi_register ;
+
+delete from vitalstatistics.chthibi_user;
+
+
+ALTER TABLE vitalstatistics.chthibi_birth AUTO_INCREMENT = 1 ;
+
+ALTER TABLE vitalstatistics.chthibi_death AUTO_INCREMENT = 1 ;
+
+ALTER TABLE vitalstatistics.chthibi_register AUTO_INCREMENT = 1 ;
+
+ALTER TABLE vitalstatistics.chthibi_user AUTO_INCREMENT = 1 ;
+
